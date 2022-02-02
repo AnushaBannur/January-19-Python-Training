2f16beb1ad50e16ba15057fb213e5fba4ce1840b
AnushaBannur <98071476+AnushaBannur@users.noreply.github.com>
2022-02-02 09:24:41 +0530

    Add files via upload

---
+++ string_slicing_type1.py
@@ -0,0 +1,25 @@
+
+#string indexing and slicing
+
+#Type1
+#starting index / ending index (both available or any one available or both unavailable)
+#index intervar not available
+#extract string in only forward manner
+
+var = "python_interpreter"
+
+print(var[1:3])
+print(var[0:5])
+print(var[-5:-2])
+print(var[:3])
+print(var[3:])
+print(var[:])
+print(var[3:10])
+print(var[2:5])
+print(var[0:10])
+print(var[5:-5])
+print(var[2:8])
+print(var[:8])
+print(var[5:])
+print(var[8:12])
+print(var[12:5])
---
+++ string_slicing_type2.py
@@ -0,0 +1,33 @@
+## Type2
+
+#starting index and ending index (both available or any one available or both unavailable)
+#index intervar should be available (only positive index intervar)
+#extract string in only forward manner
+
+var = "java_compiler_go"
+
+print(var[2:10])
+print(var[2:10:1])
+print(var[2:10:2])
+print(var[2:10:3])
+print(var[-12:10])
+print(var[-12:10:1])
+print(var[-12:10:2])
+print(var[-12:10:3])
+
+print(var[5::1])
+print(var[5::2])
+print(var[:5:1])
+print(var[:-5:1])
+print(var[-8::2])
+
+print(var[3:10:1])
+print(var[3:10:2])
+print(var[3:10:3])
+print(var[5:-5:1])
+print(var[5:-5:2])
+print(var[5:-5:5])
+print(var[:8:4])
+print(var[5::3])
+print(var[::2])
+print(var[12:5:3])
