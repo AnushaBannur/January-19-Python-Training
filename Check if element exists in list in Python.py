10dd904dcc25e338ba57860db848182e8f49a5cc
AnushaBannur <98071476+AnushaBannur@users.noreply.github.com>
2022-03-01 19:22:05 +0530

    Add files via upload

---
+++ Check if element exists in list in Python.py
@@ -0,0 +1,18 @@
+#Check if element exists in list in Python
+
+Li = [1,2,3,4,5]
+
+#using for loop
+for i in Li:
+    if (2==i):
+        print("Ele exists")
+else:
+   print("Ele does not exists")
+
+#using if block
+
+if 3 in Li:
+    print("Ele exists")
+else:
+    print("Ele does not exists")    
+
